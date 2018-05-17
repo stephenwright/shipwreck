b0db301efb3811afa9b60ed9e705f44b0fc88c93
Stephen Wright <stephen@binsentry.com>
2018-05-17 19:25:55 -0400

    added some colours and some error handling

---
+++ src/flash.js
@@ -0,0 +1,14 @@
+
+class Flash {
+  constructor(el) {
+    this.box = el;
+  }
+
+  clear() {
+    this.box.innerHTML = '';
+  }
+
+  add(msg, type) {
+    this.box.appendChild(_html(`<div class="banner ${type}">${msg}</div>`));
+  }
+}
