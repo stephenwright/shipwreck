d57c0d2fa7eb690c17ef7cf0715ce0aab7193f55
Stephen Wright <stephen@binsentry.com>
2018-05-21 14:25:50 -0400

    moved ship markup into the markup helper
    better tabs

--- src/markup.js
+++ src/markup.js
@@ -34,21 +34,10 @@ const markup = {
       return markup.entityCard(item);
     else if (item instanceof SirenLink)
       return markup.linkCard(item);
-  },
-
-  ship() {
-    return `
-      <div class="shipwreck">
-        <div class="current-path"></div>
-        <div class="tabs"></div>
-        <div class="fancy">
-          <div class="flex-parent">
-            <div class="root"></div>
-          </div>
-        </div>
-        <div class="raw"></div>
-      </div>
-    `;
+    else if (item instanceof SirenAction)
+      return markup.actionCard(item);
+    else
+      return '';
   },
 
   entityCard(entity) {
@@ -67,6 +56,10 @@ const markup = {
           <div class="entity-properties entity-raw">
             <label>properties:</label>
             ${markup.code(entity.properties)}
+          </div>
+          <div class="entity-actions">
+            <label>actions:</label>
+            ${entity.actions.map(markup.actionForm).join('\n')}
           </div>
         </div>
       </div>
@@ -92,12 +85,17 @@ const markup = {
 
   actionForm(action) {
     return `
-      <form action='${action.href}' method='${action.method}' type='${action.type}' onsubmit='return false;'>
+      <form
+        name="${action.name}"
+        type="${action.type}"
+        action="${action.href}"
+        method="${action.method}"
+        onsubmit="return false;">
         <h3>${action.name}</h3>
-        <div class='form-fields'>
+        <div class="form-fields">
           ${action.fields.map(markup.fieldForm).join('\n')}
         </div>
-        <input type='submit' value='submit'>
+        <input type="submit" value="submit">
         <p class="entity-action-href">${action.method} ${action.href}</p>
       </form>
     `;
@@ -116,6 +114,114 @@ const markup = {
           </div>
           `;
     }
+  },
+
+  actionCard(action) {
+    return `
+      <div class="card">
+        ${markup.actionForm(action)}
+      </div>
+    `;
+  },
+
+  ship(entity, target) {
+    target.innerHTML = `
+      <div class="shipwreck">
+
+        <!-- Display the current location in a nice clickable manner -->
+        <div class="current-path"></div>
+
+        <!-- Tabs to switch between raw and pretty views -->
+        <div class="tabs">
+          <a name="content-entity" class="active">Entity</a>
+          <a name="content-raw">Raw</a>
+        </div>
+
+        <!-- Pretty view of an entity -->
+        <div class="content" id="content-entity">
+          <div class="flex-parent">
+            <div class="flex-1">
+
+              <div class="entity-class" ${entity.class.length === 0 ? 'hidden': ''}>
+                <h2>Class</h2>
+                [ ${entity.class.join(', ')} ]
+              </div>
+
+              <div class="entity-links" ${entity.links.length === 0 ? 'hidden': ''}>
+                <h2>Links</h2>
+                ${entity.links.map(l => `<div>${markup.linkAnchor(l)}</div>`).join('\n')}
+              </div>
+
+              <div class="entity-properties" ${Object.keys(entity.properties).length === 0 ? 'hidden': ''}>
+                <h2>Properties</h2>
+                <table><tbody></tbody></table>
+              </div>
+
+              <div class="entity-actions">
+                <h2>Actions</h2>
+                ${entity.actions.map(markup.card).join('\n')}
+              </div>
+
+            </div>
+            <div class="flex-2" ${entity.entities.length === 0 ? 'hidden': ''}>
+
+              <div class="entity-entities">
+                <h2>Entities</h2>
+                ${entity.entities.map(markup.card).join('\n')}
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <!-- Raw JSON entity object -->
+        <div class="content" id="content-raw">
+          <div class="entity-raw">${markup.code(entity.raw)}</div>
+        </div>
+
+      </div>
+    `;
+
+    // Tabs
+    const contents = target.querySelectorAll('.shipwreck > .content');
+    const tabs = target.querySelectorAll('.shipwreck > .tabs > a');
+    tabs.forEach(tab => {
+      tab.onclick = () => {
+        tabs.forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+        contents.forEach(c => c.style.display = c.id === tab.name ? 'block' : 'none');
+      };
+    });
+
+    // Current path
+    const link = entity.link('self');
+    if (link) {
+      const url = new URL(link.href);
+      const parts = url.pathname.split('/').filter(i => i);
+      let href = url.origin;
+      target.querySelector('.current-path').innerHTML = `
+          <a href="#${href}">${href}</a> /
+          ${parts.map(p => `<a href="#${href = href + '/' + p}">${p}</a>`).join(' / \n')}
+      `;
+    }
+
+    // Display the Properties
+    const tbody = target.querySelector('.entity-properties tbody');
+    const rows = Object
+      .keys(entity.properties)
+      .forEach(k => tbody.appendChild(_html(`
+        <tr>
+          <td class="key">${k}:</td>
+          <td class="value">${entity.properties[k]}</td>
+        </tr>`
+      )));
+
+    // Make sub entity content togglable
+    target.querySelectorAll('.entity-entities > .card').forEach(card => {
+      const body = card.querySelector('.body');
+      const head = card.querySelector('.head');
+      head.onclick = () =>  body.style.display = body.style.display === 'block' ? 'none' : 'block';
+    });
   },
 
 };
--- src/shipwreck.js
+++ src/shipwreck.js
@@ -86,133 +86,22 @@ class Shipwreck {
   }
 
   async render(entity, target) {
-    // clear the content of the target output element before refilling it
-    target.innerHTML = markup.ship();
-
-    const root = target.querySelector('.root');
-    const fancy = target.querySelector('.fancy');
-    const flexParent = target.querySelector('.flex-parent');
-    const raw = target.querySelector('.raw');
-
-    // Display the raw JSON received from the API request
-    raw.innerHTML = `<div class="entity-raw">${markup.code(entity.raw)}</div>`;
-
-    // Tabs
-    const tabs = target.querySelector('.tabs');
-    const tab1 = _html('<a name="shipwreck-entity" class="active">Entity</a>');
-    const tab2 = _html('<a name="shipwreck-raw">Raw</a>');
-    tab1.onclick = () => {
-      fancy.style.display = 'block';
-      raw.style.display = 'none';
-      tab1.classList.add('active');
-      tab2.classList.remove('active');
-    };
-    tab2.onclick = () => {
-      fancy.style.display = 'none';
-      raw.style.display = 'block';
-      tab1.classList.remove('active');
-      tab2.classList.add('active');
-    };
-    tabs.appendChild(tab1);
-    tabs.appendChild(tab2);
-    raw.style.display = 'none';
-
-    // Current path
-    const link = entity.link('self');
-    if (link) {
-      const url = new URL(link.href);
-      const parts = url.pathname.split('/').filter(i => i);
-      let href = url.origin;
-      target.querySelector('.current-path').innerHTML = `
-          <a href="#${href}">${href}</a> /
-          ${parts.map(p => `<a href="#${href = href + '/' + p}">${p}</a>`).join(' / ')}
-      `;
-    }
-
-    // Entity class
-    entity.class.length !== 0 && root.appendChild(_html(`
-      <div class="entity-class">
-        <h2>Class</h2>
-        [ ${entity.class.join(', ')} ]
-      </div>
-    `));
-
-    // Display the Properties
-    if (Object.keys(entity.properties).length !== 0) {
-      const rows = Object
-        .keys(entity.properties)
-        .map(k => `
-          <tr>
-            <td class="key">${k}:</td>
-            <td class="value">${entity.properties[k]}</td>
-          </tr>`)
-        .join('\n');
-
-      root.appendChild(_html(`
-        <div class="entity-properties">
-          <h2>Properties</h2>
-          <table><tbody>${rows}</tbody></table>
-        </div>
-      `));
-    }
-
-    // Display all the Links
-    entity.links.length !== 0 && root.appendChild(_html(`
-      <div class="entity-links">
-        <h2>Links</h2>
-        ${entity.links.map(l => `<div>${markup.linkAnchor(l)}</div>`).join('\n')}
-      </div>
-    `));
-
-    // Display Entites
-    if (entity.entities.length !== 0) {
-      const entities = _html(`
-        <div class="entity-entities">
-          <h2>Entities</h2>
-        </div>
-      `);
-
-      const cards = entity.entities.forEach(e => {
-        const card = _html(markup.card(e));
-        const body = card.querySelector('.body');
-        const head = card.querySelector('.head');
-        head.onclick = () =>  body.style.display = body.style.display === 'block' ? 'none' : 'block';
-        if (e.actions.length !== 0) {
-          body.appendChild(_html('<label>actions:</label>'));
-          e.actions.forEach(a => body.appendChild(this.actionForm(a)));
+    markup.ship(entity, target);
+    // intercept action form submissions
+    target.querySelectorAll('.entity-actions form').forEach(form => {
+      form.onsubmit = () => {
+        const action = entity.action(form.name);
+        if (action) {
+          const data = {};
+          action.fields.forEach(f => data[f.name] = form.elements[f.name].value);
+          this.fetch(action, data);
         }
-        entities.appendChild(card);
-      });
-
-      flexParent.appendChild(entities);
-    }
-
-    // Display a form for each Action
-    if (entity.actions.length !== 0) {
-      const actions = _html(`
-        <div class="entity-actions">
-          <h2>Actions</h2>
-        </div>
-      `);
-      entity.actions.forEach(a => {
-        const card = _html(`<div class="card"></div>`);
-        card.appendChild(this.actionForm(a));
-        actions.appendChild(card);
-      });
-      root.appendChild(actions);
-    }
-  }
-
-  // get the markup for an action form, and attach a submission event
-  actionForm(action) {
-    const form = _html(markup.actionForm(action));
-    form.onsubmit = () => {
-      const data = {};
-      action.fields.forEach(f => data[f.name] = form.elements[f.name].value);
-      this.fetch(action, data);
-      return false;
-    };
-    return form;
+        else {
+          this._raise('error', { message: 'Could not find the entity action associated with submitted form.'});
+        }
+        return false;
+      };
+    });
   }
 
 }
