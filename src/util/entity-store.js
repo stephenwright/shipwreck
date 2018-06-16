1e234c12e82a4ccc16aca32a16bd75a6b507c108
Stephen Wright <stephen@binsentry.com>
2018-06-16 01:12:23 -0400

    linting

--- src/util/entity-store.js
+++ src/util/entity-store.js
@@ -1,7 +1,7 @@
 import EventEmitter from './event-emitter.js';
 import { SirenEntity } from '../siren';
 
-class EntityStore extends EventEmitter {
+export default class EntityStore extends EventEmitter {
   constructor() {
     super();
     this._cache = new Map();
@@ -9,7 +9,6 @@ class EntityStore extends EventEmitter {
   }
 
   async _fetch(href, token) {
-    const method = action.method || 'GET';
     const headers = new Headers();
     token && headers.set('authorization', `Bearer ${token}`);
     const response = await fetch(href, {
@@ -29,8 +28,7 @@ class EntityStore extends EventEmitter {
     let cache = this._cache.get(token);
     if (!cache) {
       this._cache.set(token, cache = new Map());
-    }
-    else if (cache.has(href)) {
+    } else if (cache.has(href)) {
       return cache.get(href);
     }
     this._raise('inflight', { count: ++this._inflight });
@@ -38,8 +36,7 @@ class EntityStore extends EventEmitter {
       const entity = await this._fetch(href, token);
       cache.set(href, entity);
       this._raise('update', { href, entity });
-    }
-    catch (error) {
+    } catch (error) {
       this._raise('error', { error });
     }
     this._raise('inflight', { count: --this._inflight });
--- src/util/event-emitter.js
+++ src/util/event-emitter.js
@@ -1,23 +1,33 @@
-class EventEmitter {
+/**
+ * EventEmitter
+ * adds the ability to emit events
+ */
+export default class EventEmitter {
   constructor() {
     this._events = new Map();
   }
 
   on(event, callback) {
     let callbacks = this._events.get(event);
-    if (!callbacks) this._events.set(event, callbacks = new Set());
+    if (!callbacks) {
+      this._events.set(event, callbacks = new Set());
+    }
     callbacks.set(callback);
   }
 
   off(event, callback) {
     let callbacks = this._events.get(event);
-    if (!callbacks) return;
+    if (!callbacks) {
+      return;
+    }
     callbacks.delete(callback);
   }
 
   _raise(event, data) {
     let callbacks = this._events.get(event);
-    if (!callbacks) return;
+    if (!callbacks) {
+      return;
+    }
     callbacks.forEach(fn => fn(data));
   }
 }
