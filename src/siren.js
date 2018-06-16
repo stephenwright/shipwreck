753ae191b1b0ec55af6f5dbd86ba4791b7e966e0
Nathan Hoel <nathanhoel@gmail.com>
2018-06-15 21:50:04 -0400

    review

--- src/siren.js
+++ src/siren.js
@@ -154,8 +154,7 @@ class SirenField extends SirenBase {
     this.class = json['class'] || [];
     this.title = json['title'] || '';
     this.type = json['type'] || 'text';
-    // output: value if truthy, 0 if 0, otherwise empty string
-    this.value = (json['value'] === 0) ? 0 : (json['value'] || '');
+    this.value = json['value'] === undefined ? '' : json['value'];
   }
 
   get json() {
